35756150c7504c7f1a1d1b78969a69afcfd62336
hikio-17 <fajri010217@gmail.com>
2023-06-11 11:11:06 -0700

    change

--- app.vue
+++ app.vue
@@ -3,8 +3,16 @@
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container d-flex justify-content-start">
-          <a class="navbar-brand" href="#">TaskList</a>
-          <a class="nav-link" href="#">Home</a>
+          <a
+            class="navbar-brand"
+            href="#"
+          >
+            TaskList
+          </a>
+          <a
+            class="nav-link"
+            href="#"
+          >Home</a>
         </div>
       </nav>
     </header>
@@ -12,49 +20,80 @@
     <main class="container mt-3">
       <h6>{{ tasks.length ? 'Task' : 'Belum ada task' }}</h6>
       <div class="list-task border-top">
-        <div v-for="(task, i) of tasks" :key="i" class="item-task d-flex align-items-start border-bottom pt-3 pb-4">
-          <input 
-            type="checkbox" 
-            name="status" 
-            id="task" 
-            class="me-2 mt-2" 
+        <div
+          v-for="(task, i) of tasks"
+          :key="i"
+          class="item-task d-flex align-items-start border-bottom pt-3 pb-4"
+        >
+          <input
+            id="task"
+            type="checkbox"
+            name="status"
+            class="me-2 mt-2"
             :checked="task.isDone"
-            @click="handleTaskIsDone(task)" 
-          />
+            @change="handleTaskIsDone(task)"
+          >
           <div class="d-flex flex-column">
-            <div :class="{ 'text-decoration-line-through': task.isDone }" class="title-task mb-1">
+            <div
+              :class="{ 'text-decoration-line-through': task.isDone }"
+              class="title-task mb-1"
+            >
               {{ task.title }}
             </div>
             <div class="description-task small text-muted">
               {{ task.description }}
             </div>
-            <a @click="handleDeleteTask(task)" href="#">Delete</a>
+            <a
+              class="text-danger"
+              @click="handleDeleteTask(task)"
+            >Delete</a>
           </div>
         </div>
       </div>
 
       <div class="action py-2">
-        <a v-if="!isCreating" @click="isCreating = !isCreating" href="#" class="add-button">Add Task</a>
-        <div v-else class="add-card">
+        <button
+          v-if="!isCreating"
+          href="#"
+          class="btn btn-info rounded-pill"
+          @click="isCreating = !isCreating"
+        >
+          Add
+          Task
+        </button>
+        <div
+          v-else
+          class="add-card"
+        >
           <div class="card mb-2">
             <div class="card-body d-flex flex-column p-0">
-              <input 
-                class="form-control border-0 mb-2" 
-                placeholder="Title" 
-                type="text" 
-                v-model="titleValue" 
-              />
-              <textarea 
-                class="form-control border-0 small" 
-                placeholder="Description" 
-                rows="3"
+              <input
+                v-model="titleValue"
+                class="form-control border-0 mb-2"
+                placeholder="Title"
+                type="text"
+              >
+              <textarea
                 v-model="descriptionValue"
-              ></textarea>
+                class="form-control border-0 small"
+                placeholder="Description"
+                rows="3"
+              />
             </div>
           </div>
           <div class="button-wrapper d-flex">
-            <button @click.prevent="handleOnSubmit" class="btn btn-primary me-2">Save</button>
-            <button @click="isCreating = !isCreating" class="btn btn-outline-secondary">Cancel</button>
+            <button
+              class="btn btn-primary me-2"
+              @click.prevent="handleOnSubmit"
+            >
+              Save
+            </button>
+            <button
+              class="btn btn-outline-secondary"
+              @click="isCreating = !isCreating"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
@@ -85,22 +124,21 @@ export default {
       isCreating: false,
       titleValue: '',
       descriptionValue: '',
+      errorMessage: '',
     };
   },
   methods: {
     handleOnSubmit() {
-      if (!this.titleValue || !this.descriptionValue) {
-        return alert('Title dan Deskripsi tidak boleh kosong!');
-      }
+      if (!this.titleValue || !this.descriptionValue) return alert('Title dan deskripsi tidak boleh kosong');
 
       const newTask = {
         title: this.titleValue,
         description: this.descriptionValue,
         isDone: false,
       };
-
+      
       this.tasks.push(newTask);
-
+      
       this.titleValue = '';
       this.descriptionValue = '';
       this.isCreating = false;
